b41e2caf4d8113c95d3adf26ddde264ab35c50d8
Rohith Kodipaka <rohithkodipaka@Rohiths-MacBook-Air.local>
2024-08-31 12:08:07 -0500

    Payment Microservice Configured and Implemented

---
+++ services/payment/src/main/java/com/rohith/ecommerce/notification/PaymentNotificationRequest.java
@@ -0,0 +1,15 @@
+package com.rohith.ecommerce.notification;
+
+import com.rohith.ecommerce.payment.PaymentMethod;
+
+import java.math.BigDecimal;
+
+public record PaymentNotificationRequest(
+        String orderReference,
+        BigDecimal amount,
+        PaymentMethod paymentMethod,
+        String customerFirstName,
+        String customerLastName,
+        String customerEmail
+) {
+}
---
+++ services/payment/src/main/java/com/rohith/ecommerce/payment/PaymentMethod.java
@@ -0,0 +1,13 @@
+package com.rohith.ecommerce.payment;
+
+public enum PaymentMethod {
+    PAY_PAL,
+
+    CREDIT_CARD,
+
+    VISA,
+
+    MASTER_CARD,
+
+    BITCOIN
+}
